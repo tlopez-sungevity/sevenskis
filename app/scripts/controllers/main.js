a3933f4e5834bb5e7ef0bc0ac4b46df538b1801c
Traci Lopez <traciklopez@gmail.com>
2016-01-06 21:48:07 -0800

    set up

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sevenSkisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sevenSkisApp
+ */
+angular.module('sevenSkisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
